c5b7bb7636825052aa8481f91bd17dae6d533dba
Rukizangabo Armand Benjamin <armandbenjamin30@gmail.com>
2025-04-26 15:49:17 +0200

    Make a show work section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './sections/hero'
+import ShowcaseSection from './sections/ShowcaseSection'
 
 export default function App() {
   return (
     <div>
       <Hero />
+      <ShowcaseSection />
     </div>
   )
 }
